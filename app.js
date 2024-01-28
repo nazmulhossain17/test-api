703c5742d79985de8e1a6827a2c3eeaef4658862
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2024-01-28 21:26:25 +0600

    cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://shoe-inventory-5714f.web.app/",
     credentials: true,
   })
 );
